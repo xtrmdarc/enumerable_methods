b644846918e1a1900838a107c481f48683fce341
xtrmdarc <diegoreyesco@hotmail.com>
2020-02-20 20:30:23 -0500

    Setup enumerator methods

---
+++ main.rb
@@ -0,0 +1,22 @@
+module Enumerator
+  def my_each
+  end
+  def my_each_with_index
+  end
+  def my_select
+  end
+  def my_all
+  end
+  def my_any
+  end
+  def my_none
+  end
+  def my_count
+  end
+  def my_map
+  end
+  def my_inject
+  end
+  def my_map
+  end
+end
