557c44f0286b6d55ddf40160578f6a4c790b60bb
xtrmdarc <diegoreyesco@hotmail.com>
2020-02-23 11:15:39 -0500

    Use While instead of For and my_inject method created

--- main.rb
+++ main.rb
@@ -1,36 +1,50 @@
+# rubocop:disable Style/CaseEquality
+
 module Enumerable
   def my_each
-    for i in (0...self.length)
-      yield(self[i])
+    i = 0
+    while i < length
+      yield self[i]
+      i += 1
     end
   end
   def my_each_with_index
-    for i in (0...self.length)
+    i = 0
+    while i < length
       yield(self[i],i)
+      i += 1
     end
   end
   def my_select
     temp = []
-    for i in (0...self.length)
+    i = 0
+    while i < length
       temp.push(self[i]) if yield(self[i])
+      i += 1
     end
     return temp
   end
   def my_all?
-    for i in (0...self.length)
+    i = 0
+    while i < length
       return false if !yield(self[i])
+      i += 1
     end
     return true
   end
   def my_any?
-    for i in (0...self.length)
+    i = 0
+    while i < length
       return true if yield(self[i])
+      i += 1
     end
     return false
   end
   def my_none?
-    for i in (0...self.length)
+    i = 0
+    while i < length
       return false if yield(self[i])
+      i += 1
     end
     return true
   end
@@ -38,13 +52,16 @@ module Enumerable
     return self.length if !filter
     temp = []
     count = 0
+    i = 0
     if block_given?
-      for i in (0...self.length)
+      while i < length
         count += 1 if yield(self[i])
+        i += 0
       end
     else
-      for i in (0...self.length)
+      while i < length
         count += 1 if self[i] == filter
+        i += 0
       end
     end
     return count
@@ -52,17 +69,43 @@ module Enumerable
   def my_map
     return self if !block_given?
     temp = []
-    for i in (0...self.length)
+    i = 0
+    while i < length
       temp.push(yield(self[i]))
+      i += 1
     end
     return temp
   end
-  def my_inject
+  def my_inject (initial = nil, sym  = nil)
+    i = 1
+    memo = self[0]
+    sym, initial = initial, sym if initial.is_a? Symbol
 
+    if initial
+      memo = initial  
+      i = 0
+    end
+    
+    if block_given?
+      while i < length
+        memo = yield memo, self[i]
+        i += 1
+      end
+    else 
+
+      return 'Please enter a Math symbol or supply a block' if !sym && !initial
+      
+      while i < length
+        memo = memo.send(sym,self[i])
+        i += 1
+      end
+    end
+    memo
   end
+
 end
 
-arr = [5,2,3]
+arr = [5,6,7,8,9,10]
 puts "my_each method output :"
 arr.my_each do |num|
   puts num.to_s
@@ -104,13 +147,20 @@ end
 puts count.to_s
 
 puts "my_map method output :"
-map = arr.map do |v|
+map = arr.my_map do |v|
   v * 3
 end
 puts map
 
 puts "my_inject method output :"
-inject = arr.inject do |v|
-  v * 3
+inject = arr.my_inject do |memo, num|
+  memo * num
 end
+
 puts inject
+
+puts "inject method output :"
+inject = arr.inject(:*)
+puts inject
+
+# rubocop:enable Style/CaseEquality
